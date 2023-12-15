03664dbbea9cb39cd4d8a48be0c15951f064240d
Nikita Slomov <kirigayaua@gmail.com>
2023-12-15 03:19:43 +0200

    add new file

---
+++ my_project_7.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int j = 7, i = 3, n = 10;
+
+    while (i < n)
+
+    {
+
+        i++;
+
+        if (i == j) continue;
+
+    }
+    cout << i;
+}
+
