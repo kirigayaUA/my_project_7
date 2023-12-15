fb7fa6d50a6d89cc129841d5f12205ddb0fbf918
Nikita Slomov <kirigayaua@gmail.com>
2023-12-15 03:29:24 +0200

    add new code

--- my_project_7.cpp
+++ my_project_7.cpp
@@ -4,17 +4,23 @@
 using namespace std;
 int main()
 {
-    int j = 7, i = 3, n = 10;
-
-    while (i < n)
-
-    {
-
-        i++;
-
-        if (i == j) continue;
+    int sum;
+    float x, Y, i;
+    cout << "input x: ";
+    cin >> x;
+    sum = 0;
+    if (x == 0) {
+        cout << "error";
+    }
+    else {
+        for (i = 0; i < 6; i++) {
+            sum = sum + pow(i, 2);
+        }
+        Y = (sum + sin(x)) / (pow(x, 2) + 5);
+        cout << "Y=" << Y;
 
     }
-    cout << i;
+}
+
 }
 
